7a49d818193a7eeb3b1fecf8cc04d9653f9911c0
mindy-zheng <mindyzheng13@gmail.com>
2022-09-20 00:18:22 -0500

    Customized the window of the calculator by adding entry box and a widget

--- calculator.py
+++ calculator.py
@@ -2,5 +2,8 @@ from tkinter import *
 
 window = Tk()
 window.geometry("400x500")
+window.title("Calculator")
+window.configure(bg="#cadce8")
+window.iconbitmap("calculator_icon.ico")
 
 window.mainloop()
