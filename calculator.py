7cb67b3293f99cc42d84b7f7d1db2ecf63246d73
Mindy Zheng <42878699+mindy-zheng@users.noreply.github.com>
2022-09-20 02:03:53 -0500

    Added full functionality to almost all buttons
    clear and backspace are still works in progress

--- calculator.py
+++ calculator.py
@@ -6,6 +6,19 @@ window.title("Calculator")
 window.configure(bg="#cadce8")
 window.iconbitmap("calculator_icon.ico")
 
+expression = ''
+
+def press(n):
+	global expression
+	expression +=str(n)
+	equation.set(expression)
+
+def equal():
+	global expression
+	total = str(eval(expression))
+	equation.set(total)
+	exression=''
+
 expression_frame = Frame(window, bg="#cadce8")
 buttons_frame = Frame(window, bg='#edf5f5')
 expression_frame.pack()
@@ -19,25 +32,29 @@ equation.set('0')
 equation_field =Entry(expression_frame, textvariable=equation, font=font_entry, justify='center')
 equation_field.pack(ipadx=15,ipady=15,pady=25)
 
-button1 = Button(buttons_frame, text='1', font=font_button, relief='groove', bg='#ffffff', borderwidth=1, width=8, height=3)
-button2 = Button(buttons_frame, text='2', font=font_button, relief='groove', bg='#ffffff', borderwidth=1, width=8, height=3)
-button3 = Button(buttons_frame, text='3', font=font_button, relief='groove', bg='#ffffff', borderwidth=1, width=8, height=3)
-plus = Button(buttons_frame, text='+', font=font_button, relief='groove', bg='#ffffff', borderwidth=1, width=8, height=3)
-button4 = Button(buttons_frame, text='4', font=font_button, relief='groove', bg='#ffffff', borderwidth=1, width=8, height=3)
-button5 = Button(buttons_frame, text='5', font=font_button, relief='groove', bg='#ffffff', borderwidth=1, width=8, height=3)
-button6 = Button(buttons_frame, text='6', font=font_button, relief='groove', bg='#ffffff', borderwidth=1, width=8, height=3)
-minus = Button(buttons_frame, text='-', font=font_button, relief='groove', bg='#ffffff', borderwidth=1, width=8, height=3)
-button7 = Button(buttons_frame, text='7', font=font_button, relief='groove', bg='#ffffff', borderwidth=1, width=8, height=3)
-button8 = Button(buttons_frame, text='8', font=font_button, relief='groove', bg='#ffffff', borderwidth=1, width=8, height=3)
-button9 = Button(buttons_frame, text='9', font=font_button, relief='groove', bg='#ffffff', borderwidth=1, width=8, height=3)
-multiply= Button(buttons_frame, text='x', font=font_button, relief='groove', bg='#ffffff', borderwidth=1, width=8, height=3)
-button0 = Button(buttons_frame, text='0', font=font_button, relief='groove', bg='#ffffff', borderwidth=1, width=8, height=3)
-decimal = Button(buttons_frame, text='.', font=font_button, relief='groove', bg='#ffffff', borderwidth=1, width=8, height=3)
+
+#Creating and customizing buttons
+button1 = Button(buttons_frame, text='1', font=font_button, relief='groove', bg='#ffffff', borderwidth=1, width=8, height=3, command=lambda: press(1))
+button2 = Button(buttons_frame, text='2', font=font_button, relief='groove', bg='#ffffff', borderwidth=1, width=8, height=3, command=lambda: press(2))
+button3 = Button(buttons_frame, text='3', font=font_button, relief='groove', bg='#ffffff', borderwidth=1, width=8, height=3, command=lambda: press(3))
+plus = Button(buttons_frame, text='+', font=font_button, relief='groove', bg='#ffffff', borderwidth=1, width=8, height=3, command=lambda: press('+'))
+button4 = Button(buttons_frame, text='4', font=font_button, relief='groove', bg='#ffffff', borderwidth=1, width=8, height=3, command=lambda: press(4))
+button5 = Button(buttons_frame, text='5', font=font_button, relief='groove', bg='#ffffff', borderwidth=1, width=8, height=3, command=lambda: press(5))
+button6 = Button(buttons_frame, text='6', font=font_button, relief='groove', bg='#ffffff', borderwidth=1, width=8, height=3, command=lambda: press(6))
+minus = Button(buttons_frame, text='-', font=font_button, relief='groove', bg='#ffffff', borderwidth=1, width=8, height=3, command=lambda: press('-'))
+button7 = Button(buttons_frame, text='7', font=font_button, relief='groove', bg='#ffffff', borderwidth=1, width=8, height=3, command=lambda: press(7))
+button8 = Button(buttons_frame, text='8', font=font_button, relief='groove', bg='#ffffff', borderwidth=1, width=8, height=3, command=lambda: press(8))
+button9 = Button(buttons_frame, text='9', font=font_button, relief='groove', bg='#ffffff', borderwidth=1, width=8, height=3, command=lambda: press(9))
+multiply= Button(buttons_frame, text='x', font=font_button, relief='groove', bg='#ffffff', borderwidth=1, width=8, height=3, command=lambda: press('*'))
+button0 = Button(buttons_frame, text='0', font=font_button, relief='groove', bg='#ffffff', borderwidth=1, width=8, height=3, command=lambda: press(0))
+decimal = Button(buttons_frame, text='.', font=font_button, relief='groove', bg='#ffffff', borderwidth=1, width=8, height=3, command=lambda: press('.'))
 clear = Button(buttons_frame, text='Clear', font=font_button, relief='groove', bg='#ffffff', borderwidth=1, width=8, height=3)
-divide = Button(buttons_frame, text='÷', font=font_button, relief='groove', bg='#ffffff', borderwidth=1, width=8, height=3)
-equal = Button(buttons_frame, text='=', font=font_button, relief='groove', bg='#ffffff', borderwidth=1, width=8, height=3)
+divide = Button(buttons_frame, text='÷', font=font_button, relief='groove', bg='#ffffff', borderwidth=1, width=8, height=3, command=lambda: press('/'))
+equal = Button(buttons_frame, text='=', font=font_button, relief='groove', bg='#ffffff', borderwidth=1, width=8, height=3, command=equal)
 backspace = Button(buttons_frame, text='⌫', font=font_button, relief='groove', bg='#ffffff', borderwidth=1, width=8, height=3)
 
+
+#Positioning buttons on the calculator
 button1.grid(row=0,column=0)
 button2.grid(row=0,column=1)
 button3.grid(row=0,column=2)
